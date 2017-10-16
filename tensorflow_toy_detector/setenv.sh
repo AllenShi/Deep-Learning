f2ca65564af1205a9875b74aef946af0c61b5471
Allen Shi <shijl@cn.ibm.com>
2017-10-16 13:37:57 +0800

    add and change pipeline config for models

---
+++ tensorflow_toy_detector/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export TF_MODEL_HOME=/Users/mobile/Projects/tensorflow/models
+export TF_MODEL_CKPT=/Users/mobile/Projects/tensorflow/CKPT
+export PYTHONPATH=$PYTHONPATH:$TF_MODEL_HOME/research:$TF_MODEL_HOME/research/slim
---
+++ tensorflow_toy_detector/train.sh
@@ -0,0 +1,3 @@
+. setenv.sh
+
+python train.py --logtostderr --pipeline_config_path=ssd_mobilenet_v1_coco.config --train_dir=build/
